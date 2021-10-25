fdc1bdaf48e6c2370976fc43653af7f17d5cbda9
vinh4501 <vinhcotam04052001@gmail.com>
2021-10-25 15:55:24 +0700

    update db

--- sql/bookshop.sql
+++ sql/bookshop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 20, 2021 lúc 03:26 PM
--- Phiên bản máy phục vụ: 10.4.21-MariaDB
--- Phiên bản PHP: 8.0.11
+-- Thời gian đã tạo: Th10 25, 2021 lúc 10:54 AM
+-- Phiên bản máy phục vụ: 10.4.19-MariaDB
+-- Phiên bản PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -70,15 +70,18 @@ CREATE TABLE `tb_user` (
   `user_name` varchar(100) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_pass` varchar(100) NOT NULL,
-  `user_status` tinyint(1) NOT NULL
+  `user_status` tinyint(1) NOT NULL,
+  `user_code` varchar(100) NOT NULL,
+  `level_user` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `tb_user`
 --
 
-INSERT INTO `tb_user` (`user_name`, `user_email`, `user_pass`, `user_status`) VALUES
-('duongle', 'duong@gmail.com', '1234', 1);
+INSERT INTO `tb_user` (`user_name`, `user_email`, `user_pass`, `user_status`, `user_code`, `level_user`) VALUES
+('admin', 'vinhvinh4501@gmail.com', '$2y$10$Y.1aViyB5pbxN1hUwP6QeeXBdqMOjIBEV0v33dGUD0fIwOeghpYD2', 1, 'c5bfb92d80bde3f62abfacc08a62c3fb', 1),
+('vinhvinhvinh', 'vinhcotam04052001@gmail.com', '$2y$10$dECGO4.3oCucp5padQPl7ukcihEw.3RVDVwBuMzWC4DBBPSTub5qG', 1, '72a5ce6ac3b432864dee5f7050d63be6', NULL);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -97,27 +100,10 @@ ALTER TABLE `tb_img`
   ADD PRIMARY KEY (`img_id`);
 
 --
--- Chỉ mục cho bảng `tb_receipt`
---
-ALTER TABLE `tb_receipt`
-  ADD PRIMARY KEY (`receipt_id`),
-  ADD KEY `user_name` (`user_name`);
-
---
 -- Chỉ mục cho bảng `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`user_name`);
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `tb_receipt`
---
-ALTER TABLE `tb_receipt`
-  ADD CONSTRAINT `tb_receipt_ibfk_1` FOREIGN KEY (`user_name`) REFERENCES `tb_user` (`user_name`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
