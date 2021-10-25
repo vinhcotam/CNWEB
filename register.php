63b920ef8a1b23c46f365c5ccffa7cf1453aba8f
vinh4501 <vinhcotam04052001@gmail.com>
2021-10-25 21:19:55 +0700

    them điều kiên của register

--- register.php
+++ register.php
@@ -50,7 +50,7 @@
                                         
                                     </div>
                                     <div class="d-flex justify-content-center">
-                                        <button type="submit" name="sbmRegister"
+                                        <button type="submit" name="sbmRegister" id="sbmRegister"
                                             class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Đăng
                                             ký</button>
                                     </div>
@@ -71,6 +71,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     </script>
+    <script src="js/checkpass.js"></script>
 </body>
 
 </html>
