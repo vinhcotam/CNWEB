bfcacdc54b9b515ad9c37c96f2de1cd10743aee9
vinh4501 <vinhcotam04052001@gmail.com>
2021-11-01 10:33:28 +0700

    push

--- user/infor.php
+++ user/infor.php
@@ -3,8 +3,6 @@
     if(!isset($_SESSION['loginOK'])){
         header("Location:../login.php"); 
     }
-    
-
 ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
