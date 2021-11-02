828281581b35ad265c928335e453bd5ddbb0b7bb
vinh4501 <vinhcotam04052001@gmail.com>
2021-11-02 21:34:51 +0700

    end project

--- logout.php
+++ logout.php
@@ -1,9 +1,15 @@
 <?php
     session_start(); //Dịch vụ bảo vệ
+    //session_destroy();
     if(isset($_SESSION['loginOK'])){
         unset($_SESSION['loginOK']);
         unset($_SESSION['admin']);
         header("Location:login.php"); 
+    }    
+    if(isset($_SESSION['admin'])){
+        unset($_SESSION['admin']);
+        header("Location:login.php"); 
     }
+    
 ?>
  
