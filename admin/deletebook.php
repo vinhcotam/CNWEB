cb1de64dd6d32c8e38c7c14549d7ae86ed548c91
vinh4501 <vinhcotam04052001@gmail.com>
2021-11-03 20:44:02 +0700

    up

--- admin/deletebook.php
+++ admin/deletebook.php
@@ -1,10 +1,10 @@
 <?php
     include 'header.php';
     include '../conf.php';
-    if(isset($_GET['id'])){
-        $id=$_GET['id'];
+    if(isset($_GET['book_id'])){
+        $id=$_GET['book_id'];
     }
-    $sql = "update tb_book set book_quantity=0 where book_id ='$id'";
+    $sql = "update tb_book set book_quantity= 0 where book_id ='$id'";
     //echo $sql;
     $result=mysqli_query($conn,$sql);
     if($result>0){
