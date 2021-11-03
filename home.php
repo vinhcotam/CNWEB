7bf21673c5e46cd5329d1a4b78fb1128c3941c0b
duong2kkq1 <levanduongl70@gmail.com>
2021-11-03 20:03:49 +0700

    sửa lỗi nhỏ khi ấn vào nút home ở trang home

--- home.php
+++ home.php
@@ -39,7 +39,7 @@
                     <!-- Left links -->
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="home.php?username = <?php echo $username?>">Home</a>
+                            <a class="nav-link" href="home.php?username=<?php echo $username?>">Home</a>
                         </li>
                         <?php if($username==''){
                         echo '<li class="nav-item">';
